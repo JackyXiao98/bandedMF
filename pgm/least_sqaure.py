9e19527fd48b778a005289dcee371f457bbf0709
Yingtai Xiao <1025965245@qq.com>
2024-04-09 17:02:02 -0400

    .

--- pgm/least_sqaure.py
+++ pgm/least_sqaure.py
@@ -5,7 +5,7 @@ from functools import reduce
 
 np.random.seed(23)
 np.set_printoptions(precision=3)
-vec_x = np.array([0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 1])
+vec_x = np.array([0, 2, 10, 0, 2, 0, 50, 0, 2, 0, 20, 1])
 ls_ab = [np.eye(2), np.eye(2), np.ones([1, 3])]
 ls_bc = [np.ones([1, 2]), np.eye(2), np.eye(3)]
 ls_ac = [np.eye(2), np.ones([1, 2]), np.eye(3)]
@@ -26,4 +26,25 @@ print("The optimal value for x is:", x.value)
 print("The L1 error for x is: ", np.linalg.norm(x.value-vec_x, 1))
 diff = Mac @ (x.value - vec_x)
 error = np.linalg.norm(diff, 1)
-print("The L1 error for Mac is: ", error)
+print("The L1 error for non-negative lsq is: ", error)
+
+ls_sum = [np.ones([1, 2]), np.ones([1, 2]), np.ones([1, 3])]
+ls_res_a = [np.array([1, -1]), np.ones([1, 2]), np.ones([1, 3])]
+ls_res_c = [np.ones([1, 2]), np.ones([1, 2]), np.array([[1, -1, 0], [1, 0, -1]])]
+ls_res_ac = [np.array([1, -1]), np.ones([1, 2]), np.array([[1, -1, 0], [1, 0, -1]])]
+
+Rsum = reduce(np.kron, ls_sum)
+Ra = reduce(np.kron, ls_res_a)
+Rc = reduce(np.kron, ls_res_c)
+Rac = reduce(np.kron, ls_res_ac)
+
+y_sum = np.sum(vec_x) + np.sqrt(2.4) * np.random.normal(size=1)
+y_res_a = Ra @ vec_x + np.sqrt(2) * np.random.normal(size=Ra.shape[0])
+y_res_c = Rc @ vec_x + np.sqrt(2) * np.random.normal(size=Rc.shape[0])
+
+y_ac_recon = Mac @ np.linalg.pinv(Rsum) @ y_sum + \
+             Mac @ np.linalg.pinv(Ra) @ y_res_a + \
+             Mac @ np.linalg.pinv(Rc) @ y_res_c
+y_ac = Mac @ vec_x
+recon_error = np.linalg.norm(y_ac_recon - y_ac, 1)
+print("The L1 error for lsq recon is: ", recon_error)
